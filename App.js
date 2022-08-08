32b3fe9b3aeba024b0bbd34fc3eca8bc0b5203a6
Asileka <asilkatya@gmail.com>
2022-08-08 13:10:59 +0100

    push

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Reviviiiiiiiiiiiivedggrh apppp</Text>
+      <Text>Reviviiiiiiiiiiii apppp</Text>
       <StatusBar style="auto" />
     </View>
   );
