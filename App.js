d226ce6667ea167bf1f69181a03d276cadc38d5b
Asileka <asilkatya@gmail.com>
2022-08-09 10:43:10 +0100

    push2

--- App.js
+++ App.js
@@ -1,29 +1,51 @@
 import { StatusBar } from "expo-status-bar";
-import { DatePickerAndroid, StyleSheet, Text, View } from "react-native";
+import {
+  DatePickerAndroid,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from "react-native";
 import { Avatar } from "@rneui/themed";
 import Avatars from "./components/Avatars";
 import SignInPage from "./components/SignInPage";
 import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 3000);
 
-export default function App() {
-  const [loaded] = useFonts({
-    Montserrat: require("./assets/fonts/Pacifico-Regular.ttf"),
-  });
+const Stack = createNativeStackNavigator();
 
-  if (!loaded) {
-    return null;
-  }
+function App() {
   return (
-    <View style={styles.container}>
-      <SignInPage />
-
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Sign In">{<SignInPage />}</Stack.Screen>
+        <Stack.Screen name="reVive" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -35,3 +57,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+export default App;
