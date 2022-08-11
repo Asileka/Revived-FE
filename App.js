74c004c123ba14afb0c2946666a5b89f88b5c0de
Asileka <asilkatya@gmail.com>
2022-08-11 16:10:24 +0100

    usercontext

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ import AllItems from "./components/AllItems";
 import ListItem from "./components/ListItem";
 import SignIn from "./components/SignIn";
 import EditUsername from "./components/EditProfile";
+import { userContext } from "./components/Contexts";
 function DetailsScreen() {
   return (
     <View
@@ -54,44 +55,46 @@ function MainItemsPage({ navigation }) {
 
 function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        id="1"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#0d8575",
-          },
-          headerTintColor: "#edf7f6",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-        initialRouteName="All Items"
-      >
-        <Drawer.Screen
-          style={{ backgroundColor: "black" }}
-          name="All Items"
-          component={MainItemsPage}
-        />
+    <userContext.Provider value={"62f4ca92859022833060019b"}>
+      <NavigationContainer>
+        <Drawer.Navigator
+          id="1"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#0d8575",
+            },
+            headerTintColor: "#edf7f6",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+          initialRouteName="All Items"
+        >
+          <Drawer.Screen
+            style={{ backgroundColor: "black" }}
+            name="All Items"
+            component={MainItemsPage}
+          />
 
-        <Drawer.Screen name="My Profile" component={MyProfile} />
-        <Drawer.Screen name="List Item" component={ListItem} />
+          <Drawer.Screen name="My Profile" component={MyProfile} />
+          <Drawer.Screen name="List Item" component={ListItem} />
 
-        <Drawer.Screen name="Inbox" component={DetailsScreen} />
-        <Drawer.Screen name="Categories" component={DetailsScreen} />
-        <Drawer.Screen name="Favourites" component={DetailsScreen} />
-        <Drawer.Screen name="Edit Profile" component={EditUsername} />
-        <Drawer.Screen
-          name="Other Users Profile"
-          component={OtherUsersProfile}
-        />
+          <Drawer.Screen name="Inbox" component={DetailsScreen} />
+          <Drawer.Screen name="Categories" component={DetailsScreen} />
+          <Drawer.Screen name="Favourites" component={DetailsScreen} />
+          <Drawer.Screen name="Edit Profile" component={EditUsername} />
+          <Drawer.Screen
+            name="Other Users Profile"
+            component={OtherUsersProfile}
+          />
 
-        <Drawer.Screen name="Map" component={MyMap} />
-        <Drawer.Screen name="Terms" component={DetailsScreen} />
-        <Drawer.Screen name="Sign In" component={SignIn} />
-        <Drawer.Screen name="Log Out" component={DetailsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+          <Drawer.Screen name="Map" component={MyMap} />
+          <Drawer.Screen name="Terms" component={DetailsScreen} />
+          <Drawer.Screen name="Sign In" component={SignIn} />
+          <Drawer.Screen name="Log Out" component={DetailsScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </userContext.Provider>
   );
 }
 
